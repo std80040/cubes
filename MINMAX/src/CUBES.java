6b6dfa05a7e90f3a9140f415b12693e1e402f345
user <user@HPPC>
2022-04-09 12:52:51 +0300

    update Cubes class

--- MINMAX/src/CUBES.java
+++ MINMAX/src/CUBES.java
@@ -1,9 +1,8 @@
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 import java.util.Scanner;
 import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.TimeUnit;
 
 public class CUBES {
   
@@ -12,19 +11,26 @@ public class CUBES {
 	public static void main(String[] args) {
 	
 	
-	Scanner scanner= new Scanner(System.in);   
-	System.out.println("Number of cubes:");	
+	Scanner scanner= new Scanner(System.in);  
+	System.out.println("Set number of cubes on the table:");	
 	
 	int cubesNo=scanner.nextInt();
 	
 	
-	System.out.println("Choose Κ cubes to reset:");
+	System.out.println("Set number of cubes to take from the table except 1 or 2 :");
 	int kCubes=scanner.nextInt();
-	int [] resCubes={1,2,kCubes};
+	//int [] resCubes={1,2,kCubes};
 	
 	List<CUBE> board = createBoard(cubesNo);
+	System.out.println();
+	
+	System.out.println("GAME START !!!");
+	System.out.println();
+	
 	printBoard(board);
 	System.out.println();
+	System.out.println();
+	
 	
 		MinMax playerMax=new MinMax();
     
@@ -47,8 +53,16 @@ public class CUBES {
 	}
 	
 	
+	System.out.print("PC PLAYS");
+	for(int i=1;i<=3;i++) {
+		System.out.print(".");
+		delay(1);
+	}
 	
+	System.out.println();
 	System.out.println("PC take "+maxMove+" Cubes from the table");
+	System.out.println();
+	
 	playerMove(board,maxMove);
 	if(board.size()==0) {
 	System.out.println("THE WINNER IS PC !!!");
@@ -58,47 +72,49 @@ public class CUBES {
 	     printBoard(board);
 	     System.out.println();
 	     }
-	
+	System.out.println();
 	System.out.println("You can take (1 or 2 or "+ kCubes+") cubes from the table :");
 	int resCubesNo=scanner.nextInt();
 	
+	//Έλεγχος εισαγωγής μη αποδεκτών τιμών (1,2,Κ)
 	while ((resCubesNo !=1 && resCubesNo !=2 && resCubesNo !=kCubes)){ 
 		
-		if(resCubesNo !=1 && resCubesNo !=2 && resCubesNo !=kCubes) {
 		  System.out.println("You are not allowed to take "+resCubesNo+" Cubes from the table !");
+		  System.out.println();
 		  printBoard(board);
-		  
-		}
-		if(resCubesNo > board.size()) {
+		  System.out.println();
+		  System.out.println("Take (1 or 2 or "+ kCubes+") cubes :");
+	      resCubesNo=scanner.nextInt();
+	}
+		
+		//Ελεγχος εισαγωγής τιμής>αριθμός κύβων στο τραπέζι
+		while(resCubesNo > board.size()) {
 		 System.out.println();
 		 System.out.println("You try to take more Cybes than the board !");
-		 printBoard(board);
-		}
-		System.out.println();
-		System.out.println("Take (1 or 2 or "+ kCubes+") cubes :");
-		resCubesNo=scanner.nextInt();
-		
-	}
-	while(resCubesNo > board.size()) {
 		 System.out.println();
-		 System.out.println("You try to take more Cybes than the board have !");
 		 printBoard(board);
+		 System.out.println();
+		 System.out.println("Take (1 or 2 or "+ kCubes+") cubes :");
 		 resCubesNo=scanner.nextInt();
 		}
+		
+
 	
-	
+	System.out.println();
 	playerMove(board,resCubesNo);
 	if(board.size()==0) {
 		System.out.println("THE WINNER IS THE PLAYER !!!");
 		break;
 		}
 		else {
-	         printBoard(board);
+		     printBoard(board);
 	         System.out.println();
 	         }		
 	
 	}
 	
+		scanner.close();
+		
 	}//main-----------------------------------------
 
 	
@@ -115,6 +131,11 @@ public class CUBES {
 		for(int i=0;i<=board.size()-1;i++) {
 		System.out.print(board.get(i).getCube());
 		}
+		System.out.println();
+		    for(int i=1;i<=board.size()*4;i++) {
+			System.out.print("=");
+			}
+	
 	}
 	
 	public static void playerMove(List<CUBE> board, int resetCubes) {
@@ -126,5 +147,14 @@ public class CUBES {
 	public static int getRandomElement(int[] arr){
 	    return arr[ThreadLocalRandom.current().nextInt(arr.length)];
 	}
+	
+	public static void delay(int sec) {
+		try {
+		    TimeUnit.SECONDS.sleep(sec);
+	        } catch (InterruptedException e) {
+		            e.printStackTrace();
+	                }
+	}
+	
 	
 }
